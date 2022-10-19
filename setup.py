702582f923bf050c7cf749d583185a2b86b8b045
ERijck <emil.rijcken@gmail.com>
2022-10-19 19:12:39 +0200

    Changed version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 HERE = pathlib.Path(__file__).parent
 
-VERSION = '2.0.0'
+VERSION = '2.0.1'
 PACKAGE_NAME = 'FuzzyTM'
 AUTHOR = 'Emil Rijcken'
 AUTHOR_EMAIL = 'emil.rijcken@gmail.com'
